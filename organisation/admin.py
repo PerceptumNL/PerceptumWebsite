66544da3f6955e069a08c430c66023e45b383a82
mslatour <sanderlatour@gmail.com>
2014-03-29 12:48:32 +0100

    Added fieldsets to member and project

--- organisation/admin.py
+++ organisation/admin.py
@@ -11,6 +11,15 @@ class OrganisationAdmin(admin.ModelAdmin):
             'facebook', 'twitter', 'linkedin', 'youtube', 'pinterest')})
     )
 
+class MemberAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('active', )}),
+        ('About', {'fields': (
+            'name', 'tagline', 'description', 'img')}),
+        ('Social networks', {'fields': (
+            'facebook', 'twitter', 'linkedin', 'youtube', 'pinterest')})
+    )
+
 class MemberInlineAdmin(admin.TabularInline):
     model = Member.projects.through
     extra = 1
@@ -18,11 +27,17 @@ class MemberInlineAdmin(admin.TabularInline):
     verbose_name_plural = "Members"
 
 class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('active', )}),
+        ('About', {'fields': ('title', 'description', 'categories')}),
+        ('Images', {'fields': ('small_img', 'large_img')}),
+        ('Links', {'fields': ('website', 'repository')}),
+    )
     inlines = (MemberInlineAdmin,)
     model = Project
 
 # Register your models here.
 admin.site.register(Organisation, OrganisationAdmin)
-admin.site.register(Member)
+admin.site.register(Member, MemberAdmin)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(ProjectCategory)
