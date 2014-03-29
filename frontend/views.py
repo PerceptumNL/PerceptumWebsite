c5a44983a75b570f6335fbbe8546052a700e2c0c
mslatour <sanderlatour@gmail.com>
2014-03-29 03:17:02 +0100

    Added interface selector to view

--- frontend/views.py
+++ frontend/views.py
@@ -5,8 +5,16 @@ from frontend.models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {
-        "interface": Interface.objects.filter(active=True)[0],
+    interface_id = request.GET.get("interface", None)
+    if interface_id is None:
+        interface = Interface.objects.filter(active=True)[0]
+    else:
+        try:
+            interface = Interface.objects.get(pk=interface_id)
+        except:
+            raise Http404
+    return render(request, interface.template, {
+        "interface": interface,
         "organisation": Organisation.objects.all()[0],
         "members": Member.objects,
         "projects": Project.objects,
